afcd47f41d95bae6afeed240d3871d742d595e40
v3yu <v3yu@ucsd.edu>
2025-08-28 00:32:02 -0700

    ???

--- mm_agents/gui_agent_wrapper.py
+++ mm_agents/gui_agent_wrapper.py
@@ -1,32 +1,30 @@
-import argparse
 from agent.agent import FunctionCallAgent
-from tools.gui_tools import ClickTool, TypeTool, ScrollTool, WaitTool, StopTool, PressKeyTool, PageGotoTool
-from tools.analysis_tools import MapSearchTool, ContentAnalyzerTool
-from tools.web_search_tools import WebSearchTool
-
-"""Wrapper class for GUI-Agent for compatibility with OSWorld evaluation"""
+from qwen_agent.tools import (
+    ClickTool, TypeTool, ScrollTool, WaitTool, StopTool, PressKeyTool, PageGotoTool,
+    MapSearchTool, ContentAnalyzerTool, WebSearchTool
+)
 
 class PatchedFunctionCallAgent(FunctionCallAgent):
     def _define_functions(self):
-        # Patch tool names for correct registration
+        # Return tool instances with correct names
         click_tool = ClickTool(); click_tool.name = "click"
         type_tool = TypeTool(); type_tool.name = "type"
+        press_key_tool = PressKeyTool(); press_key_tool.name = "press_key"
         scroll_tool = ScrollTool(); scroll_tool.name = "scroll"
         wait_tool = WaitTool(); wait_tool.name = "wait"
         stop_tool = StopTool(); stop_tool.name = "stop"
-        press_key_tool = PressKeyTool(); press_key_tool.name = "press_key"
         map_search_tool = MapSearchTool(); map_search_tool.name = "map_search"
         content_analyzer_tool = ContentAnalyzerTool(); content_analyzer_tool.name = "content_analyzer"
         page_goto_tool = PageGotoTool(); page_goto_tool.name = "goto_url"
-        # web_search_tool = WebSearchTool(); web_search_tool.name = "web_search" # Uncomment if needed
+        # web_search_tool = WebSearchTool(); web_search_tool.name = "google_web_search" # Uncomment if needed
 
         return [
             click_tool,
             type_tool,
+            press_key_tool,
             scroll_tool,
             wait_tool,
             stop_tool,
-            press_key_tool,
             map_search_tool,
             content_analyzer_tool,
             page_goto_tool,
@@ -34,16 +32,12 @@ class PatchedFunctionCallAgent(FunctionCallAgent):
         ]
 
 class GUIAgentWrapper:
-    def __init__(self, args: argparse.Namespace):
+    def __init__(self, args):
         self.agent = PatchedFunctionCallAgent(args)
         self.trajectory = []
         self.meta_data = {}
         self.intent = ""
         self.args = args
-
-        # Patch function_map directly to ensure registration
-        tool_list = self.agent._define_functions()
-        self.agent.function_map = {tool.name: tool for tool in tool_list}
 
     def predict(self, instruction, obs, **kwargs):
         self.intent = instruction
