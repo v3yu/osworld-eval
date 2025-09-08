c1e90a87269a9aed7c45c20da9c3a58d4f6cbf2c
v3yu <v3yu@ucsd.edu>
2025-09-08 02:12:14 -0700

    x

--- mm_agents/GUI-Agent-main/actions/__init__.py
+++ mm_agents/GUI-Agent-main/actions/__init__.py
@@ -1,5 +1,26 @@
+"""Actions module for the GUI Agent"""
+from .action_creator import (
+    create_click_action,
+    create_type_action,
+    create_scroll_action,
+    create_wait_action,
+    create_stop_action,
+    create_key_press_action,
+    create_goto_url_action,
+    create_none_action,
+    create_select_action
+)
 from .help_functions import parse_action_json
 
 __all__ = [
+    'create_click_action',
+    'create_type_action', 
+    'create_scroll_action',
+    'create_wait_action',
+    'create_stop_action',
+    'create_key_press_action',
+    'create_goto_url_action',
+    'create_none_action',
+    'create_select_action',
     'parse_action_json'
 ] 
